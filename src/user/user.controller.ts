1c4551ea190ab17efb72a44c805153b431e8cb4a
Gilson Ricardo Peloso <gilson.peloso@webxnet.com.br>
2024-11-21 09:50:22 -0300

    User - update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common'
+import { Get, Controller } from '@nestjs/common'
+import { UserService } from './user.service'
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly usersService: UserService) {}
+    @Get()
+    getAll() {
+        return this.usersService.getAll()
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common'
+import { DrizzleService } from '@/drizzle/drizzle.service'
+import { users } from '@/drizzle/schema'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private readonly drizzleService: DrizzleService) {}
+
+    getAll() {
+        return this.drizzleService.db.select().from(users)
+    }
+}
